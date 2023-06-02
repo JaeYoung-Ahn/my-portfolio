4dbc59cc66ec514425d305093cbf8d140e15c0c6
JaeYoung-Ahn <ooctrl88@gmail.com>
2023-06-01 20:59:51 -0700

    first commit

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,39 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title: 'Airbnb',
+        category: 'web',
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: 'Movie App',
+        category: 'app',
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: 'Brand design',
+        category: 'design',
+    },
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'web',
+    },
+    {
+        name: 'app',
+    },
+    {
+        name: 'design'
+    },
+];
